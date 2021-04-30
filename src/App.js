374eab968a327695978b31d0f70a4da748d56275
Kdabrowa6@gmail.com <5z4l3j@gmail.com>
2021-04-30 12:08:11 +0200

    Wykonane laboratorium nr 8

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
-import './components/posts.jsx'
-import './components/postsTable.jsx'
+import { Route, Switch } from "react-router-dom";
 import Posts from "./components/posts";
+import {Redirect} from 'react-router'
+import Home from "./components/home";
+import NavBar from "./components/navbar";
+import NotFound from "./components/notFound";
 
 function App() {
-
-  return (<Posts/>
-  );
+    return (
+        <div className="container-fluid">
+            <NavBar />
+            <div className="container">
+                <div className="content">
+                    <Switch>
+                        <Route path="/posts"
+                               render={props => <Posts sortBy="newest" {...props} />}
+                        />
+                        <Route path="/" exact component={Home} />
+                        <Route path="/not-found" exact component={NotFound}/>
+                        <Redirect to="/not-fount"/>
+                    </Switch>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
